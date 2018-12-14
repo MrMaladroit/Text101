31d05672539d3e1fe21c223fc43016dc2d7e9a66
MrMaladroit <jamesbeckjr@gmail.com>
2018-12-14 08:33:36 -0500

    Organized Assets / New States
    Organized states into folder
    Organized scripts into folder
    Added Title and GameOver states

--- Text101/Assets/Scripts/StoryTeller.cs
+++ Text101/Assets/Scripts/StoryTeller.cs
@@ -4,14 +4,14 @@ using UnityEngine.UI;
 public class StoryTeller : MonoBehaviour
 {
     [SerializeField] private Text storyTextDisplay;
-    [SerializeField] private State openingState;
+    [SerializeField] private State titleState;
 
     private State state;
 
     // Use this for initialization
     void Start()
     {
-        state = openingState;
+        state = titleState;
         storyTextDisplay.text = state.GetStateStory();
     }
 
